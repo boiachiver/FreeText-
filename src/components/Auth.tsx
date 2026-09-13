428d4384aa4e57fc458b0abfa7891e1e25615863
boiachiver <achiverboi@gmail.com>
2026-09-13 10:44:06 +0100

    Add Auth component for authentication display

---
+++ src/components/Auth.tsx
@@ -0,0 +1,8 @@
+export default function Auth() {
+  return (
+    <div style={{ padding: "40px", textAlign: "center" }}>
+      <h1>FreeText</h1>
+      <p>Authentication is working.</p>
+    </div>
+  );
+}
